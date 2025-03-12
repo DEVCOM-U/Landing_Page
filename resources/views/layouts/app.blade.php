ed613384e26316b626f7ea58ea1c829be294d495
Firza Hakim <firzahakimcrack@gmail.com>
2025-03-12 15:52:43 +0700

    feat: navbar component

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+
+    {{-- alpine js --}}
+    <script src="https://cdn.jsdelivr.net/npm/alpinejs@3.10.2/dist/cdn.min.js" defer></script>
+
+    @vite('resources/css/app.css')
+</head>
+<body>
+    <x-navbar />
+
+    @yield('content')
+      
+</body>
+</html>
