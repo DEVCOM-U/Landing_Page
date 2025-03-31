d4959500e00c596b2f3038c9aeb0f7043d1e767f
Firza Hakim <firzahakimcrack@gmail.com>
2025-03-31 21:31:25 +0700

    feat: activity card component

---
+++ resources/views/pages/activity-pages.blade.php
@@ -0,0 +1,13 @@
+<x-layout.app>
+    <x-activity-card image="img/gedung.png" title="Activity Pertama"
+        description="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente maiores deleniti ipsa numquam rem quas perferendis eius qui alias nisi voluptatum animi earum, amet quo beatae! Consectetur recusandae numquam quia Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto corporis odio eum molestiae ducimus tempore asperiores ad nesciunt corrupti est veritatis optio, unde ullam saepe hic a provident ipsam aperiam."
+        link="https://linkedin.com/in/firzahakim" />
+
+    <x-activity-card image="img/pohon.png" title="Activity Kedua"
+        description="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente maiores deleniti ipsa numquam rem quas perferendis eius qui alias nisi voluptatum animi earum, amet quo beatae! Consectetur recusandae numquam quia Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto corporis odio eum molestiae ducimus tempore asperiores ad nesciunt corrupti est veritatis optio, unde ullam saepe hic a provident ipsam aperiam."
+        link="https://linkedin.com/in/firzahakim" />
+
+    <x-activity-card image="img/kuda.jpg" title="Activity Ketiga"
+        description="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente maiores deleniti ipsa numquam rem quas perferendis eius qui alias nisi voluptatum animi earum, amet quo beatae! Consectetur recusandae numquam quia Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto corporis odio eum molestiae ducimus tempore asperiores ad nesciunt corrupti est veritatis optio, unde ullam saepe hic a provident ipsam aperiam."
+        link="https://linkedin.com/in/firzahakim" />
+</x-layout.app>
