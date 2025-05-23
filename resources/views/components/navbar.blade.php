0cc546a90d3fc72311c2202c30b5bc325170325c
Firza Hakim <firzahakimcrack@gmail.com>
2025-05-23 21:52:39 +0700

    Feat: footer component

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,4 +1,4 @@
-<nav class="bg-white sticky dark:bg-gray-900 w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
+<nav class="bg-white sticky w-full z-20 top-0 left-0 border-b border-gray-200">
     <div class="max-w-screen-xl flex items-center justify-between mx-auto p-4">
         <!-- Logo di kiri -->
         <a href="{{ url('/') }}" class="flex items-center">
@@ -21,7 +21,7 @@
         <div x-data="{ open: false }" class="md:hidden">
             <button 
                 @click="open = !open"
-                class="p-2 w-10 h-10 text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                class="p-2 w-10 h-10 text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
             >
                 <span class="sr-only">Open main menu</span>
                 <svg class="w-6 h-6" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24">
@@ -33,7 +33,7 @@
             <div 
                 x-show="open"
                 @click.away="open = false"
-                class="absolute left-0 top-full w-full bg-white dark:bg-gray-900 shadow-md md:hidden"
+                class="absolute left-0 top-full w-full bg-white shadow-md md:hidden"
             >
                 <ul class="flex flex-col text-center space-y-2 p-4">
                     <li><a href="{{ url('/') }}" class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-200 focus:ring">Home</a></li>
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,7 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Landing Page Devcom</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
+
 
     {{-- alpine js --}}
     <script src="https://cdn.jsdelivr.net/npm/alpinejs@3.10.2/dist/cdn.min.js" defer></script>
@@ -16,5 +18,6 @@
 
     @yield('content')
       
+    <x-footer />
 </body>
 </html>
