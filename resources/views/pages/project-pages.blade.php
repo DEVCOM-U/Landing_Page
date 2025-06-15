7374a0a7e90b7c0473259c9513532893ced73c1f
Firza Hakim <firzahakimcrack@gmail.com>
2025-06-15 15:26:14 +0700

    Feat: Detail Project & Detail Activity Pages

--- resources/views/pages/project-pages.blade.php
+++ resources/views/pages/project-pages.blade.php
@@ -1,36 +1,11 @@
 <x-layout.app>
-    <x-project-card image="img/kuda.jpg" title="Project Pertama" description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae sed doloribus dolore, illo est consectetur ipsa saepe minus deserunt deleniti. Perspiciatis enim voluptatum suscipit tempora eos? Temporibus consectetur expedita minus! Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut laboriosam perspiciatis officia sunt dolore! Mollitia, totam libero. Tempore commodi sint ut, cupiditate in, tempora, nesciunt recusandae vitae quia quae earum." :profiles="[
-        ['src' => 'img/firza.jpg', 'alt' => 'Firza Ketoprak'],
-        ['src' => 'img/firza.jpg', 'alt' => 'Firza Kebab'],
-        ['src' => 'img/firza.jpg', 'alt' => 'Firza Wangsaf'],
-    ]"
-        link="https://linkedin.com/in/firzahakim" />
-
-    <x-project-card image="https://images.unsplash.com/photo-1523240795612-9a054b0db644?auto=format&fit=crop&w=1471&q=80"
-        title="Project Kedua"
-        description=" Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe eligendi, non odio iure nostrum labore sapiente deserunt ullam modi earum ipsum voluptas accusantium et nobis! Modi asperiores incidunt ipsum enim!"
-        :profiles="[
-            ['src' => 'img/firza.jpg', 'alt' => 'Firza katanya mirip jepri nikol'],
-            ['src' => 'img/firza.jpg', 'alt' => 'Firza juga'],
-            ['src' => 'img/firza.jpg', 'alt' => 'Masih firza'],
-        ]" link="https://youtube.com" />
-
-    <x-project-card image="img/gedung.png"
-        title="Project Kedua"
-        description=" Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe eligendi, non odio iure nostrum labore sapiente deserunt ullam modi earum ipsum voluptas accusantium et nobis! Modi asperiores incidunt ipsum enim!"
-        :profiles="[
-            ['src' => 'img/firza.jpg', 'alt' => 'Firza katanya mirip jepri nikol'],
-            ['src' => 'img/firza.jpg', 'alt' => 'Firza juga'],
-            ['src' => 'img/firza.jpg', 'alt' => 'Masih firza'],
-        ]" link="https://youtube.com" />
-
-    <x-project-card image="img/pohon.png"
-        title="Project Kedua"
-        description=" Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe eligendi, non odio iure nostrum labore sapiente deserunt ullam modi earum ipsum voluptas accusantium et nobis! Modi asperiores incidunt ipsum enim!"
-        :profiles="[
-            ['src' => 'img/firza.jpg', 'alt' => 'Firza katanya mirip jepri nikol'],
-            ['src' => 'img/firza.jpg', 'alt' => 'Firza juga'],
-            ['src' => 'img/firza.jpg', 'alt' => 'Masih firza'],
-        ]" link="https://youtube.com" />
+    @foreach ($projects as $slug => $project)
+        <x-project-card 
+            :image="$project['image']" 
+            :title="$project['title']"
+            :description="$project['short_description']"
+            :profiles="$project['profiles']"
+            :link="route('project.detail', ['slug' => $slug])" 
+        />
+    @endforeach
 </x-layout.app>
-
