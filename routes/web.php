280252ea048d80dc6077c9e3530796f40ac75ecb
Firza Hakim <firzahakimcrack@gmail.com>
2025-06-15 14:39:41 +0700

    Update Navbar

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('pages.home');
 });
+
+Route::get('/activity', function () {
+    return view('pages.activity-pages');
+});
+
+Route::get('/projects', function () {
+    return view('pages.project-pages');
+});
